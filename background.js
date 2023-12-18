76334b16d22bdd4f6b7e47bbb9cfb53cec45e2ef
angelahe101 <35946460+angelahe101@users.noreply.github.com>
2023-12-17 21:31:52 -0600

    deleted unnecessary files

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // set initial values upon installation
 chrome.runtime.onInstalled.addListener(function(){
-    chrome.storage.sync.set({interval: 20, on: 1});
+    chrome.storage.sync.set({interval: 0.25, on: 1});
 });
 
 // set the timer
@@ -11,8 +11,41 @@ function reset_timer(){
     time_zero = Date.now();
 }
 
+//create target websites
+const target_websites = [
+    "https://amazon.com/*",
+    "https://walmart.com/*",
+    "https://costco.com/*",
+    "https://target.com/*",
+    "https://instacart.com/*",
+    "https://sayweee.com/*",
+    "https://www.goodeggs.com/*",
+    "https://doordash.com/*",
+    "https://retailmenot.com/*",
+    "https://groupon.com/*",
+    "https://www.latimes.com/coupon-codes/*",
+    "https://slickdeals.net/*",
+    "https://coupons.com/*",
+    "https://couponfollow.com/*",
+    "https://couponcabin.com/*",
+    "https://thekrazycouponlady.com/*",
+    "https://dealsplus.com/*"
+];
+
 //create function to check timer
 function check_timer(initial_time, lapsed_time)
+    chrome.tabs.query({current: true}, function(tabs)){
+        var url = tabs[0].url;
+        for (const target_website of target_websites) {
+            if (url.match(new RegExp(target_website))) {
+                console.log("Target website found:, proceed with timer", url);
+            break
+            }
+        }
+        if (!target_website) {
+            console.log("Not a target website, skipping timer")
+        }
+    }
     if ((initial_time + lapsed_time) < Date.now()) {
         var time = new Date()
         var notification = {
@@ -26,13 +59,17 @@ function check_timer(initial_time, lapsed_time)
      reset_timer();
     
      //clear notification after 30 seconds
-     settime(function() {
+     setTimeout(function() {
         chrome.notifications.clear("Is this the best bang for your time?");
      }, 30000);
     }
 
 //reset the timer when chrome restarts
 chrome.runtime.onStartup.addListener(reset_timer);
+
+
+
+
 
 
 
