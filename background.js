6575f31e6453f2addb29df419651764aba083982
angelahe101 <35946460+angelahe101@users.noreply.github.com>
2023-12-18 22:32:15 -0600

    add icon to manifest

--- background.js
+++ background.js
@@ -1,45 +1,57 @@
 // set initial values upon installation
 chrome.runtime.onInstalled.addListener(function(){
     chrome.storage.sync.set({interval: 0.25, on: 1});
+    console.log("initial values set");
 });
 
+//retrieve stored intervals
+interval_minutes = chrome.storage.sync.get('interval', function(data) {
+    console.log("set interval for first time", data.interval);
+    return data.interval;
+});
 // set the timer
 var time_zero = 0;
+console.log("timer set")
 
 //create function to reset timer
 function reset_timer(){
+    console.log("function to reset timer created");
     time_zero = Date.now();
 }
-
-//create target websites
-const target_websites = [
-    "https://amazon.com/*",
-    "https://walmart.com/*",
-    "https://costco.com/*",
-    "https://target.com/*",
-    "https://instacart.com/*",
-    "https://sayweee.com/*",
-    "https://www.goodeggs.com/*",
-    "https://doordash.com/*",
-    "https://retailmenot.com/*",
-    "https://groupon.com/*",
-    "https://www.latimes.com/coupon-codes/*",
-    "https://slickdeals.net/*",
-    "https://coupons.com/*",
-    "https://couponfollow.com/*",
-    "https://couponcabin.com/*",
-    "https://thekrazycouponlady.com/*",
-    "https://dealsplus.com/*"
-];
 
 //create function to check timer
 function check_timer(initial_time, lapsed_time) {
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
         var url = tabs[0].url;
-        for (const target_website of target_websites) {
+        console.log("query active tab and retrieve url of active tab");
+        //create target websites
+        const target_websites = [
+            "https://amazon.com/*",
+            "https://walmart.com/*",
+            "https://costco.com/*",
+            "https://target.com/*",
+            "https://instacart.com/*",
+            "https://sayweee.com/*",
+            "https://www.goodeggs.com/*",
+            "https://doordash.com/*",
+            "https://retailmenot.com/*",
+            "https://groupon.com/*",
+            "https://www.latimes.com/coupon-codes/*",
+            "https://slickdeals.net/*",
+            "https://coupons.com/*",
+            "https://couponfollow.com/*",
+            "https://couponcabin.com/*",
+            "https://thekrazycouponlady.com/*",
+            "https://dealsplus.com/*"
+        ];
+        //initialize target_website variable
+        var target_website = null; 
+
+        for (const target of target_websites) {
             if (url.match(new RegExp(target_website))) {
                 console.log("Target website found:, proceed with timer", url);
-            break;
+                target_website = target;
+                break;
             }
         }
 
@@ -54,18 +66,26 @@ function check_timer(initial_time, lapsed_time) {
                 title: "Is this the best bang for your time?",
                 message: "You've spent $2.67 (twenty minutes) looking for a cheap deal. Do you want to keep on comparison shopping?"
             };
-
-            chrome.notifications.create("Is this the best bang for your time?");
-            console.log("timer reset");
+            chrome.notifications.create("notification_id", notification, function(notification_id) {})
+            console.log("create notification id")
+            //reset timer
             reset_timer();
+            console.log("timer reset");
     
             //clear notification after 30 seconds
             setTimeout(function() {
-                chrome.notifications.clear("Is this the best bang for your time?");
+                chrome.notifications.clear("notification");
             }, 30000);
         }
+        //call check_timer again for continuous execution
+        setTimeout(function() {
+            check_timer(time_zero,data.interval * 60000);
+        }, 20 * 60 * 1000); //20 minutes in seconds
     });
 }
+
+//call check_timer to start continuous execution
+check_timer(time_zero, 0);
 
 
 
